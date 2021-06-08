bb118f81e334b054efa1e005e3dd42f8c3e5aafa
Parrotapocalypse <olivermtgcat@gmail.com>
2021-06-07 20:45:00 -0400

    Add project files.

---
+++ FriendlyHelper/Entities/CustomHeart.cs
@@ -0,0 +1,12 @@
+using Celeste.Mod.Entities;
+using Monocle;
+using System;
+
+namespace Celeste.Mod.FriendlyHelper.Entities
+{
+    [CustomEntity("FriendlyHelper/CustomHeart")]
+    public class CustomHeart : Entity
+    {
+        
+    }
+}
---
+++ FriendlyHelper/FriendlyHelperModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Celeste;
+using Celeste.Mod.FriendlyHelper.Entities;
+
+namespace Celeste.Mod.FriendlyHelper
+{
+    public class FriendlyHelperModule : EverestModule
+    {
+        public static FriendlyHelperModule Instance;
+
+        public FriendlyHelperModule()
+        {
+            Instance = this;
+        }
+        public override void Load()
+        {
+            CustomHeart.Load();
+        }
+
+        public override void Unload()
+        {
+            CustomHeart.Unload();
+        }
+    }
+}
